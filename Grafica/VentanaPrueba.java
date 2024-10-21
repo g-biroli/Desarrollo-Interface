956344c1aeae5e5d61740057bebb76c750588d10
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-21 22:16:02 +0200

    VentanaGrafica

---
+++ Grafica/VentanaPrueba.java
@@ -0,0 +1,47 @@
+package VentanaGrafica;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class VentanaPrueba extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrueba frame = new VentanaPrueba();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPrueba() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 653, 446);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setBounds(71, 68, 85, 21);
+		contentPane.add(btnNewButton);
+	}
+}
