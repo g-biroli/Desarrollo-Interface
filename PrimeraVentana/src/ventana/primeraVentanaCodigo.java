8ea45c410e1a086a2a964d581ac3a8d8d4b520a5
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-29 15:20:06 +0100

    Clase Ventana Grafica

---
+++ PrimeraVentana/src/ventana/primeraVentanaCodigo.java
@@ -0,0 +1,16 @@
+package ventana;
+
+import javax.swing.JFrame;
+
+public class primeraVentanaCodigo  extends JFrame {
+	
+	public primeraVentanaCodigo() {
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+	}
+	
+	
+}
