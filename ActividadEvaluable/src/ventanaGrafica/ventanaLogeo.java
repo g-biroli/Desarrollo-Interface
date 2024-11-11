dd281d83bafe0b163eda5b5d22ffcbc14a950375
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-11-11 23:15:16 +0100

    Clase Desarrollo interface - Actividad Evaluable

---
+++ ActividadEvaluable/src/ventanaGrafica/ventanaLogeo.java
@@ -0,0 +1,89 @@
+package ventanaGrafica;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+
+public class ventanaLogeo extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ventanaLogeo frame = new ventanaLogeo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ventanaLogeo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 681, 387);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("USUARIO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(42, 65, 88, 33);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblContrasea = new JLabel("CONTRASEÑA");
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblContrasea.setBounds(42, 151, 120, 33);
+		contentPane.add(lblContrasea);
+		
+		JLabel lblCargo = new JLabel("CARGO");
+		lblCargo.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblCargo.setBounds(42, 231, 120, 33);
+		contentPane.add(lblCargo);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		comboBox.setBounds(176, 239, 151, 21);
+		comboBox.addItem("Selecione una opcion");
+		comboBox.addItem("Alumno");
+		comboBox.addItem("Profesor");
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("LIMPIAR");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.setBounds(190, 296, 127, 21);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(176, 74, 151, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(176, 160, 151, 19);
+		contentPane.add(passwordField);
+	}
+}
