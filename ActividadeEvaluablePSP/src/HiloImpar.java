e3a77577d03d87517b93d371ac41f6400461ca24
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-11-15 17:29:43 +0100

    Actividad Evaluable PSP - Hilos

---
+++ ActividadeEvaluablePSP/src/HiloImpar.java
@@ -0,0 +1,19 @@
+
+public class HiloImpar implements Runnable {
+		//Implements la clase que permite usar hilo
+	
+	@Override
+	public void run() {
+		// metodo para imprimir numero Impar
+		int numeroImpar = 0;
+		
+		System.out.println("Numeros Ímpares: ");
+		//bucle para imprimir numeros pares y calcular la suma
+		for (int i = 1; i <= 10; i +=2) {
+			System.out.println(i);
+			numeroImpar += i;
+		}
+		System.out.println("La suma de los números impares: " + numeroImpar);
+	}
+
+}
---
+++ ActividadeEvaluablePSP/src/ProgramaHilos.java
@@ -0,0 +1,9 @@
+
+public class ProgramaHilos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
